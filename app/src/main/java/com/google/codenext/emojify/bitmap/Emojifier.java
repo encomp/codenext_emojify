fc7da8d0dc82f2131f49abf38228ce94c48869f7
encomp <edgar.martinez.rico@gmail.com>
2020-02-12 20:15:54 -0800

    Exercise 2 Detect Faces.

---
+++ app/src/main/java/com/google/codenext/emojify/bitmap/Emojifier.java
@@ -0,0 +1,21 @@
+package com.google.codenext.emojify.bitmap;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+
+public final class Emojifier {
+
+  // TODO (1): Define a log TAG
+
+  /**
+   * Method for detecting faces in a bitmap.
+   *
+   * @param context The application context.
+   * @param picture The picture in which to detect the faces.
+   */
+  public static void detectFaces(Context context, Bitmap picture) {
+    // TODO (2): Add the code that detects the number of faces in a given bitmap.
+    // TODO (3): Log the number of faces found on the bitmap.
+    // TODO (4): If there are no faces on a bitmap show a Toast message.
+  }
+}
