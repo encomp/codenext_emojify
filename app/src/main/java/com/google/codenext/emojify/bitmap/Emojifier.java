7f11c38bf9fb98e3930d1caef62a4a90f935e038
encomp <edgar.martinez.rico@gmail.com>
2020-02-12 20:48:48 -0800

    Exercise 2 Detect Faces Solution.

--- app/src/main/java/com/google/codenext/emojify/bitmap/Emojifier.java
+++ app/src/main/java/com/google/codenext/emojify/bitmap/Emojifier.java
@@ -2,10 +2,23 @@ package com.google.codenext.emojify.bitmap;
 
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.util.SparseArray;
+import android.widget.Toast;
 
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.face.Face;
+import com.google.android.gms.vision.face.FaceDetector;
+import com.google.codenext.emojify.R;
+
+import timber.log.Timber;
+
+/** Utility class that detect the number of faces on a {@link Bitmap} image. */
 public final class Emojifier {
 
-  // TODO (1): Define a log TAG
+  private Emojifier() {}
+
+  // Define a log TAG
+  private static final String TAG = Emojifier.class.getSimpleName();
 
   /**
    * Method for detecting faces in a bitmap.
@@ -14,8 +27,29 @@ public final class Emojifier {
    * @param picture The picture in which to detect the faces.
    */
   public static void detectFaces(Context context, Bitmap picture) {
-    // TODO (2): Add the code that detects the number of faces in a given bitmap.
-    // TODO (3): Log the number of faces found on the bitmap.
-    // TODO (4): If there are no faces on a bitmap show a Toast message.
+    // Add the code that detects the number of faces in a given bitmap.
+    // Create a FaceDetector that identifies the faces of people in a Bitmap graphic object.
+    FaceDetector detector =
+            new FaceDetector.Builder(context)
+                    .setTrackingEnabled(false)
+                    .setClassificationType(FaceDetector.ALL_CLASSIFICATIONS)
+                    .build();
+
+    // Create a Frame instance from the bitmap to supply to the detector.
+    Frame frame = new Frame.Builder().setBitmap(picture).build();
+
+    // Detect the all the faces on a frame.
+    SparseArray<Face> faces = detector.detect(frame);
+
+    // Log the number of faces found on the bitmap.
+    Timber.tag(TAG).d("The number of faces found are: " + faces.size());
+
+    // If there are no faces on a bitmap show a Toast message.
+    if (faces.size() == 0) {
+      Toast.makeText(context, R.string.no_faces_message, Toast.LENGTH_SHORT).show();
+    }
+
+    // Release the detector to free up native resources that are no longer needed.
+    detector.release();
   }
 }
